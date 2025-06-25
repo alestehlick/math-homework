afe40d9b43617a4a38c01e8dfa59555aa7dcbc2c
alestehlick <57234187+alestehlick@users.noreply.github.com>
2025-06-25 23:00:04 +0900

    Update core.js

--- assets/core.js
+++ assets/core.js
@@ -8,7 +8,7 @@
 
 /* 1️⃣  PASTE your Google-Apps-Script Web-App URL (must end “/exec”) */
 const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbyWXzr0GdOOeoFyRvLhLnODvSqCXMxxCtKYCXvr7XvjAongKFEAfdtTAdIyiL6p-UwC/exec";
+  "https://script.google.com/macros/s/AKfycbwv-WoPU049MK9SNr6J-nmeM8_UqTEO54JzrH4AtqBkYPoWOEofi3LWHm8i0IS7aqW-gA/exec";
 
 /* 2️⃣  Cool-down between submissions (milliseconds) */
 const COOLDOWN_MS = 120_000;          // 2 minutes
